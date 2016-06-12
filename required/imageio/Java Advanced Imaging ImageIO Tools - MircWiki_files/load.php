9971e32de10a36720b2769df939551a96399fddd
homer-simpleton <noone.knows@nowhere.xyz>
2016-06-12 19:26:51 -0400

    Remove all jai and imagio requirements.

--- required/imageio/Java Advanced Imaging ImageIO Tools - MircWiki_files/load.php
+++
@@ -1,8 +0,0 @@
-/*
-Internal error
-
-Internal error
-
-Problematic modules: {"startup":"error"}
-*/
-if (window.console && console.error) {console.error("Internal error\n\nInternal error\n\nProblematic modules: {\"startup\":\"error\"}");}
