8643ac6c93d240d4eecbd32c924d4e2bd827bbaf
oakridge <julianklein@gmail.com>
2024-09-17 09:32:17 -0400

    Update Tensorflow lib for TF 2.16.1 support and CUDA 12.2

--- lib-tf/src/test/java/ai/kognition/pilecv4j/tf/TfTest.java
+++ lib-tf/src/test/java/ai/kognition/pilecv4j/tf/TfTest.java
@@ -7,13 +7,15 @@ import java.io.File;
 import java.util.Arrays;
 
 import org.junit.Test;
+import org.junit.Ignore;
 import org.opencv.core.CvType;
 import org.opencv.imgproc.Imgproc;
 import org.tensorflow.SavedModelBundle;
 import org.tensorflow.Session;
 import org.tensorflow.Session.Runner;
+import org.tensorflow.TensorFlow;
 import org.tensorflow.Tensor;
-import org.tensorflow.proto.framework.ConfigProto;
+import org.tensorflow.proto.ConfigProto;
 import org.tensorflow.types.TFloat32;
 
 import ai.kognition.pilecv4j.image.CvMat;
@@ -25,8 +27,11 @@ import ai.kognition.pilecv4j.image.display.ImageDisplay;
 // Interrogate the saved model in order to get the input/output
 // Operation names using the process described here:
 // https://stackoverflow.com/questions/59263406/how-to-find-operation-names-in-tensorflow-graph
+// import org.tensorflow.Graph;
+// import org.tensorflow.GraphOperation;
+// import java.util.Iterator;
 
-//@Ignore
+@Ignore
 public class TfTest {
 
     // public static final String MODEL = "/data/jim/kog/data/EV-models.testing/saved_ev_model_efficientnetv2-xl-21k";
@@ -41,6 +46,8 @@ public class TfTest {
 
     @Test
     public void test() throws Exception {
+
+        System.out.println("TensorFlow version: " + TensorFlow.version());
 
         final ConfigProto.Builder b = ConfigProto.newBuilder();
 
@@ -71,6 +78,8 @@ public class TfTest {
             final Session session = mb.session();
             final ImageDisplay id = new ImageDisplay.Builder().build();) {
 
+            System.out.println("Model loaded");
+
             // final Graph graph = mb.graph();
             // final Iterator<GraphOperation> itr = graph.operations();
             // while(itr.hasNext()) {
@@ -81,6 +90,7 @@ public class TfTest {
             final File dir = new File(IMAGE_DIR);
             while(true) {
                 Arrays.stream(dir.listFiles()).forEach(f -> {
+                    System.out.println("Testing: " + f.getAbsolutePath());
 
                     try(final CvMat mat = uncheck(() -> ImageFile.readMatFromFile(f.getAbsolutePath()));
                         final LetterboxDetails lbd = Utils.letterbox(mat, MODEL_INPUT_DIM);
@@ -89,6 +99,8 @@ public class TfTest {
                         CvMat ready = maybe.isContinuous() ? CvMat.shallowCopy(maybe) : CvMat.deepCopy(maybe);
                         Tensor tensor = TensorUtils.toTensor(ready, TFloat32.class);) {
 
+                        System.out.println("Loaded into tensor");
+
                         id.update(lbd.mat());
 
                         // Interrogate the saved model in order to get the input/output
@@ -96,11 +108,13 @@ public class TfTest {
                         // https://stackoverflow.com/questions/59263406/how-to-find-operation-names-in-tensorflow-graph
                         final Runner runner = session.runner();
 
+                        System.out.println("Running model");
                         final var result = runner.feed("serving_default_input_1:0", tensor)
                             // .fetch("dense/kernel:0")
                             .fetch("StatefulPartitionedCall:0")
                             .run();
 
+                        System.out.println("Extracting results");
                         System.out.println(result.get(0).shape());
                         System.out.println(Arrays.toString(TensorUtils.getVector(result.get(0))));
                     }
