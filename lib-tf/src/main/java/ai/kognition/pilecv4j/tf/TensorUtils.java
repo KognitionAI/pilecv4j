0e806100df94a4ef36489edd6dc4759b8393b05d
Matias Klein <matias.klein@kognition.ai>
2024-12-09 19:39:30 -0500

    update tensorflow to 1.0.0

--- lib-tf/src/main/java/ai/kognition/pilecv4j/tf/TensorUtils.java
+++ lib-tf/src/main/java/ai/kognition/pilecv4j/tf/TensorUtils.java
@@ -28,7 +28,7 @@
  import org.tensorflow.Tensor;
  import org.tensorflow.ndarray.Shape;
  import org.tensorflow.ndarray.buffer.DataBuffers;
- import org.tensorflow.framework.GraphDef;
+ import org.tensorflow.proto.framework.GraphDef;
  import org.tensorflow.types.TFloat32;
  import org.tensorflow.types.family.TType;
  
